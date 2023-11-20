0f3ca463869c36249f5edb45c7fc46a26583a24d
giangimw <g.nguyen@vehikl.com>
2023-11-20 07:13:34 -0500

    changed to mysql

--- database/migrations/2023_10_23_125021_create_books_table.php
+++ database/migrations/2023_10_23_125021_create_books_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('books', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->string('title');
             $table->string('author');
             $table->string('publisher');
